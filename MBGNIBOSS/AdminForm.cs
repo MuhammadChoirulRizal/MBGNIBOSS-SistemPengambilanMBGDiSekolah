f261e571236e87104b87ddbbbcdae5c699f0af9d
MuhammadChoirulRizal <mchoirulrizal9@gmail.com>
2026-04-19 14:47:56 +0700

    membuat button tambah dan void admin form

--- MBGNIBOSS/AdminForm.cs
+++ MBGNIBOSS/AdminForm.cs
@@ -256,6 +256,95 @@ MessageBoxIcon.Question);
         }
 
         // ================= FORM LOAD =================
-       
+        private void AdminForm_Load(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = null;
+            LoadStatistik();
+        }
+
+
+        void ClearForm()
+        {
+            txtNIS.Clear();
+            txtNama.Clear();
+            txtKelas.Clear();
+            txtAlergi.Clear();
+            txtTotalData.Clear();
+            txtSudah.Clear();
+            txtBelum.Clear();
+            txtStatus.Clear();
+
+            txtNIS.Focus();
+        }
+
+        private void btnTambah_Click_1(object sender, EventArgs e)
+        {
+            // ================= TAMBAH DATA =================
+
+
+            DialogResult jawab = MessageBox.Show(
+       "Yakin ingin Menambah data?",
+       "Konfirmasi",
+       MessageBoxButtons.YesNo,
+       MessageBoxIcon.Question);
+
+            if (jawab == DialogResult.No)
+                return;
+
+
+            try
+            {
+                if (txtNIS.Text.Trim() == "" ||
+                    txtNama.Text.Trim() == "")
+                {
+                    MessageBox.Show("Data belum lengkap!");
+                    return;
+                }
+
+                conn.Open();
+
+                // CEK NIS SUDAH ADA / BELUM
+                SqlCommand cek = new SqlCommand(
+                "SELECT COUNT(*) FROM Pengambilan WHERE NIS=@nis", conn);
+
+                cek.Parameters.AddWithValue("@nis", txtNIS.Text.Trim());
+
+                int jumlah = (int)cek.ExecuteScalar();
+
+                if (jumlah > 0)
+                {
+                    MessageBox.Show("NIS sudah ada!");
+                    conn.Close();
+                    return;
+                }
+
+                SqlCommand cmd = new SqlCommand(
+                "INSERT INTO Pengambilan " +
+                "(NIS,Nama,Kelas,Alergi,Tanggal,Jam,Status) " +
+                "VALUES (@nis,@nama,@kelas,@alergi,@tgl,@jam,@status)", conn);
+
+                cmd.Parameters.AddWithValue("@nis", txtNIS.Text.Trim());
+                cmd.Parameters.AddWithValue("@nama", txtNama.Text.Trim());
+                cmd.Parameters.AddWithValue("@kelas", txtKelas.Text.Trim());
+                cmd.Parameters.AddWithValue("@alergi", txtAlergi.Text.Trim());
+                cmd.Parameters.AddWithValue("@tgl", DateTime.Now.ToString("yyyy-MM-dd"));
+                cmd.Parameters.AddWithValue("@jam", DateTime.Now.ToString("HH:mm:ss"));
+                cmd.Parameters.AddWithValue("@status", "Belum Diambil");
+
+                cmd.ExecuteNonQuery();
+
+                conn.Close();
+                MessageBox.Show("Data berhasil ditambah");
+
+                LoadData();
+                ClearForm();
+                LoadStatistik();
+            }
+            catch (Exception ex)
+            {
+                conn.Close();
+                MessageBox.Show(ex.Message);
+            }
+        }
     }
 }
