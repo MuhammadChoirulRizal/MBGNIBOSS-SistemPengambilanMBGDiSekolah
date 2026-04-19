134ff6b832a840ab815e535162449068922bfc67
MuhammadChoirulRizal <mchoirulrizal9@gmail.com>
2026-04-19 14:53:34 +0700

    membuat button cari dan button proses

--- MBGNIBOSS/PetugasForm.cs
+++ MBGNIBOSS/PetugasForm.cs
@@ -41,8 +41,110 @@ namespace MBGNIBOSS
                 MessageBox.Show(ex.Message);
             }
         }
-      
 
+        private void btnCari_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conn.Open();
+
+                SqlCommand cmd = new SqlCommand(
+                "SELECT * FROM Pengambilan WHERE NIS=@nis", conn);
+
+                cmd.Parameters.AddWithValue("@nis", txtCari.Text.Trim());
+
+                SqlDataReader rd = cmd.ExecuteReader();
+
+                if (rd.Read())
+                {
+                    txtNama.Text = rd["Nama"].ToString();
+                    txtKelas.Text = rd["Kelas"].ToString();
+                    txtAlergi.Text = rd["Alergi"].ToString();
+                    txtStatus.Text = rd["Status"].ToString();
+                    txtTanggal.Text = rd["Tanggal"].ToString();
+                    txtJam.Text = rd["Jam"].ToString();
+
+                    // VALIDASI SUDAH AMBIL
+                    if (txtStatus.Text == "Sudah Diambil")
+                    {
+                        MessageBox.Show("Jatah Sudah Diambil!");
+                        btnProses.Enabled = false;
+                    }
+                    else
+                    {
+                        btnProses.Enabled = true;
+                    }
+
+                    // VALIDASI ALERGI
+                    if (txtAlergi.Text != "")
+                    {
+                        MessageBox.Show("PERHATIAN! SISWA MEMILIKI ALERGI");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("NIS tidak ditemukan");
+                }
+
+                rd.Close();
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                conn.Close();
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
+        private void btnProses_Click(object sender, EventArgs e)
+        {
+            DialogResult jawab = MessageBox.Show(
+"Yakin ingin Memproses data?",
+"Konfirmasi",
+MessageBoxButtons.YesNo,
+MessageBoxIcon.Question);
+
+            if (jawab == DialogResult.No)
+                return;
+            try
+            {
+                conn.Open();
+
+                SqlCommand cmd = new SqlCommand(
+                "UPDATE Pengambilan SET " +
+                "Status='Sudah Diambil'," +
+                "Tanggal=@tgl," +
+                "Jam=@jam " +
+                "WHERE NIS=@nis", conn);
+
+                cmd.Parameters.AddWithValue("@tgl",
+                DateTime.Now.ToString("yyyy-MM-dd"));
+
+                cmd.Parameters.AddWithValue("@jam",
+                DateTime.Now.ToString("HH:mm:ss"));
+
+                cmd.Parameters.AddWithValue("@nis",
+                txtCari.Text.Trim());
+
+                cmd.ExecuteNonQuery();
+
+                conn.Close();
+
+                MessageBox.Show("Pengambilan berhasil!");
+
+                LoadData();
+                ClearForm();
+            }
+            catch (Exception ex)
+            {
+                conn.Close();
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
+       
     }
     
 }
